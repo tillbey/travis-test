bdd1cff342276f302f01d84642ffcbaef6489a38
Till Bey <tillbey@gmail.com>
2017-08-11 15:55:59 +0200

    correct package name

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='travis-test',
+    name='travistest',
     version='0.1.0',
     author='Till Bey',
     author_email='tillbey@gmail.com',
